b7b9442620ca1ef4fffa01f0c5082b241540e780
Diana219 <81695038+Diana219@users.noreply.github.com>
2021-07-01 04:07:41 +0300

    добавлено задание 2-2

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,38 @@
+﻿#define _USE_MATH_DEFINES
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+
+double GetFirst(const double x);
+double GetSecond(const double x);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	const double a = 2.5;
+	double x;
+	cout << "Введите число x \n";
+	cin >> x;
+	if (x > a)
+	{
+		auto first = GetFirst(x);
+		cout << first;
+	}
+	else
+	{
+		auto second = GetSecond(x);
+		cout << second;
+	}
+}
+
+double GetFirst(double x) {
+	return x * pow(sin(x), 2);
+}
+double GetSecond(double x) {
+	return x * pow(cos(x), 2);
+}
