e5d404ab7f0b97b1828cbbf9b3203826d7dc188a
Diana219 <81695038+Diana219@users.noreply.github.com>
2021-07-01 04:04:18 +0300

    добавлено задание 1-2

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+double getDistance(const double x1, const double y1,
+    const double x2, const double y2)
+{
+    return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2)); /*Функция вычисления расстояния*/
+}
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    cout << "Введите координаты точки А" << endl; /*следует ввод пользователем координат точки А*/
+    double x1, y1;
+    cin >> x1 >> y1;
+    cout << "Введите координаты точки B" << endl; /*следует ввод пользователем координат точки B*/
+    double x2, y2;
+    cin >> x2 >> y2;
+    cout << "Расстояние между точками: ";
+    double distance = getDistance(x1, y1, x2, y2); /*Вызов функции*/
+    cout << distance;
+    return 0;
+}
